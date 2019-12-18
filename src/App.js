15f6d3267b8d4d66a8ac94a2fb3bd9d0aa55c4e6
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2019-12-17 22:47:29 -0500

    api keys and calls

--- src/App.js
+++ src/App.js
@@ -5,29 +5,27 @@ import './App.css';
 
 //import the title of sites from components
 import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
-// function App() {
-//   return (
-//     <div className="App">
-     
-//       <h1>
-//         hello
-//       </h1>
-//     </div>
-//   );
-// }
+const API_KEY = "8b60410aeeb38245c7fde0afbabc11b9";
 
-// export default App;
-
+// `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+// http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22
 class App extends Component {
   render(){
 
     // it can only one single element
     return (
-      <div>< Titles />
+      <div>
+        < Titles />
+        < Form />
+        < Weather />
       </div>
     )
   };
 }
 
 export default App;
+
+
